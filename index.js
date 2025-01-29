1177ccd2abdc3d9e11e8f2f80da928974168779b
Jay Davis <jdlikesbsbl@gmail.com>
2025-01-29 09:30:15 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Karen";
+
+function upperCaseCustomerName(){
+    customerName =  customerName.toUpperCase();
+}
+
+function setBestCustomer (){
+    bestCustomer = `not ${customerName.toLowerCase()}`;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = `maybe ${customerName.toLowerCase()}`
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Karma";
+}
